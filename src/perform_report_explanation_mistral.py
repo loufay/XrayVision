0d4c80ac6ae03d3d45b01b206cb3d277c55a5394
clemence-mottez <115174132+clemence-mottez@users.noreply.github.com>
2025-02-15 20:35:20 -0800

    Update perform_report_explanation_mistral.py

--- src/perform_report_explanation_mistral.py
+++ src/perform_report_explanation_mistral.py
@@ -1,5 +1,25 @@
-from models.model_loader import load_mistral_model  
+import sys
+sys.path.append('home/cmottez/aiXperts/src/models')
+from models.model_loader import load_mistral_model
 import torch
+
+
+# # Use a pipeline as a high-level helper
+# from transformers import pipeline
+
+# messages = [
+#     {"role": "user", "content": "Who are you?"},
+# ]
+# pipe = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
+# pipe(messages)
+
+# # Load model directly
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
+# model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
+
+
 
 def get_mistral_model():
     return load_mistral_model()
@@ -12,6 +32,7 @@ def explain_radiology_report(report_text):
     mistral_tokenizer, mistral_model = get_mistral_model()
     
     prompt = f"<s>[INST] Explain the following radiology report in simple and short terms for a non-medical person:\n\n{report_text}\n\nExplanation: [/INST]"
+    # prompt = f"<s>[INST] Explain the following chest x-ray report in simple and short terms for a non-medical person:\n{report_text}[/INST]"
     
     inputs = mistral_tokenizer(prompt, return_tensors="pt").to("cuda")
     
@@ -19,4 +40,10 @@ def explain_radiology_report(report_text):
         output = mistral_model.generate(**inputs, max_new_tokens=300, temperature=0.7, top_p=0.9)
     
     explanation = mistral_tokenizer.decode(output[0], skip_special_tokens=True)
+
+    start = explanation.find('Explanation')   # Find the first newline after the prompt part and adjust if your output format is different
+    explanation = explanation[start:].strip()  # Strip leading and trailing whitespace
+
     return explanation
+
+
